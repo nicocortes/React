61797399a2e53c883df7a5b8bc657d54f239a112
Nico Cortes <ncortescarbajo@gmail.com>
2021-07-12 08:35:26 +0000

    Initial commit

---
+++ intro-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+
+const title = <h1>Hola React</h1>;
+// que quiero renderiza r
+
+ReactDOM.render(title, document.getElementById('root'));
